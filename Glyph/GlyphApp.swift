02744a40a53d0d109b3e46c98647772e99a8c64a
Charles <charles@ratlabs.xyz>
2026-03-18 14:58:05 -0500

    Initial Commit

---
+++ Glyph/GlyphApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlyphApp.swift
+//  Glyph
+//
+//  Created by Carlo Shin on 3/18/26.
+//
+
+import SwiftUI
+
+@main
+struct GlyphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
